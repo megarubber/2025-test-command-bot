7f578cf6cfbc5a7bb7673c41b6deec3c557ac7ee
Guilherme Samuel <guisamuel800@gmail.com>
2025-09-06 17:20:59 -0300

    setup constants and pyproject.toml files

--- constants.py
+++ constants.py
@@ -1,2 +1,9 @@
-class OIConstants:
-    kDriverControllerPort = 0
+from wpilib import SerialPort
+
+kDriverControllerPort = 0
+kLeftMotor1Port = 1
+kLeftMotor2Port = 2
+kRightMotor1Port = 3
+kRightMotor2Port = 4
+kBaudRate = 9600
+kUSBPort = SerialPort.Port.kUSB1
